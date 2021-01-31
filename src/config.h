a766d971ae349ed16df7ff1c4a830b8ef3cced1e
Simon Angell <simon.angell307@gmail.com>
2021-01-31 20:41:46 +1100

    Fix queue issues

--- src/config.h
+++ src/config.h
@@ -10,6 +10,7 @@ const int MAX_SPLIT_FLAPS = 8;
 const int NUMBER_OF_FLAPS = 50;
 const int STEPS_PER_REVOLUTION = 200;
 const int MAX_CHARACTER_DISPLAY_QUEUE = 10;
+const int DEFAULT_PAUSE_MS = 4000;
 
 // Pinouts
 const int SR_DRIVER_LATCH_PIN = 21;
